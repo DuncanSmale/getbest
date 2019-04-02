3d7943ad4363dcbc408ffc6da34222970a6f3bd5
DuncanSmale <duncan.smale@gmail.com>
2019-04-02 13:39:57 +0200

    added comment to getbest.py

--- src/getbest.py
+++ src/getbest.py
@@ -19,6 +19,7 @@ def findTop(f,num_col, mark_col):
     best = best_idx =  0
     for line in f:
         data = line.strip().split(",")
+        #this gives errors in test case but not when the program runs
         mark = int(data[mark_col+1])
         if mark > best:
             best=mark
