67062c8bd3232c38c824ebfc4f65f22e62c5d9f4
DuncanSmale <duncan.smale@gmail.com>
2019-04-02 13:36:41 +0200

    fixing test_marks

--- src/test_marks.py
+++ src/test_marks.py
@@ -5,10 +5,10 @@ class testMarks(unittest.TestCase):
 
         def cols(self):
                 cols = [1,1]
-                testcols = getbest.getCols("../data/bestdat0.csv")
+                testcols = getbest.getCols("data/bestdat0.csv")
                 self.assertEquals(cols,testcols)
 
         def mark(self):
-                bestindex,bestmark = getbest.findtop("../data/bestdat0.csv",4,1)
+                bestindex,bestmark = getbest.findtop("data/bestdat0.csv",4,1)
                 self.assertEquals(bestmark,90)
         
