f32e493fad7708c86190066f5ac0324e8e24ce76
DuncanSmale <duncan.smale@gmail.com>
2019-04-02 13:10:09 +0200

    added test cases

---
+++ src/test_marks.py
@@ -0,0 +1,14 @@
+import unittest
+import getbest
+
+class testMarks(unittest.TestCase):
+
+        def cols(self):
+                cols = [1,1]
+                testcols = getbest.getCols("../data/bestdat0.csv")
+                self.assertEquals(cols,testcols)
+
+        def mark(self):
+                bestindex,bestmark = getbest.findtop("../data/bestdat0.csv",4,1)
+                self.assertEquals(bestmark,90)
+        
